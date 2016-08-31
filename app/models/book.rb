4cc3ea355c21f93e63a00954678cf331027c1386
Md. Dinar <dinar.cse@gmail.com>
2016-09-01 04:16:16 +0600

    add book image with paperclip

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :category
+
+	has_attached_file :book_img, styles: { book_index: "250x350>", book_show: "335x475>"}
+  	validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
+  	validates :book_img, attachment_presence: true
 end

69f735025cb07bafc40e3ecdff185c2d3b32ba8b
Md. Dinar <dinar.cse@gmail.com>
2016-09-02 01:34:35 +0600

    fixed route file

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, :skip => [:registrations]
   namespace :admin do
     resources :users
-<<<<<<< HEAD
-  resources :books
-  resources :categories
-=======
-	  resources :books
-	  resources :categories
-    resources :admin_users
->>>>>>> user-features
+    resources :books
+    resources :categories
+	  resources :admin_users
 
     root to: "books#index"
   end
